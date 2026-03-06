8b5de0f13f9efda26eb5a3ae453681333fdb2653
waller.jannik <jannik.waller@uni-konstanz.de>
2026-03-06 11:38:57 +0100

    Gui start

---
+++ Projects/test.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+label1 = tk.Label(root, text='Hallo Welt')
+label2 = tk.Label(root, text='Zweite Reihe')
+
+
+label1.pack()
+label2.pack()
+
+root.mainloop()
